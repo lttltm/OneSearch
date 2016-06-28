e19ff948beab0ecaff388a9e16011c2fb9f4cc33
tanyl8511 <tanyl@tcl.com>
2016-06-28 13:33:10 +0800

    change jar to war

--- src/main/java/com/lttltm/fans/onesearch/Application.java
+++ src/main/java/com/lttltm/fans/onesearch/Application.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.core.env.Environment;
 
 
@@ -16,10 +18,14 @@ import org.springframework.core.env.Environment;
  */
 
 @SpringBootApplication
-public class Application 
-{
+public class Application extends SpringBootServletInitializer {
 	
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(Application.class);		
+	} 
 	
     public static void main( String[] args ) throws UnknownHostException
     {
